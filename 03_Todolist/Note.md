5b34e214b0b7e23c3190496c45388f79b44c4f4b
I'm XiaoZhi <gcz9956@outlook.com>
2023-07-05 23:50:35 +0800

    update

---
+++ 03_Todolist/Note.md
@@ -0,0 +1,6 @@
+# 知识点
+
+1. 父子组件之间通信
+   父传子 `props` 传递
+   子传父 通过父传递给子的函数
+2. `defaultChecked` 和 `checked` 区别，类似的还是 `defaultValue` 和 `value`
--- src/App.jsx
+++ src/App.jsx
@@ -1,108 +1,13 @@
-import React, { Component, useState } from "react";
+import React, { Component } from "react";
 import { nanoid } from "nanoid"; // 类似uuid的 生成随机id
 import "./App.css";
 
-// 创建外壳组件
-// export default class App extends Component {
-//   state = {
-//     line: [
-//       [0, 1, 2],
-//       [3, 4, 5],
-//       [6, 7, 8],
-//       [0, 3, 6],
-//       [1, 4, 7],
-//       [2, 5, 8],
-//       [0, 4, 8],
-//       [2, 4, 6],
-//     ],
-//   };
-//   render() {
-//     return (
-//       <div className="game_area">
-//         <div className="grid-top">X</div>
-//         <div className="grid-top"></div>
-//         <div className="grid-top">O</div>
-//         <div className="grid-middle"></div>
-//         <div className="grid-middle"></div>
-//         <div className="grid-middle"></div>
-//         <div className="grid-bottom"></div>
-//         <div className="grid-bottom"></div>
-//         <div className="grid-bottom"></div>
-//       </div>
-//     );
-//   }
-// }
-
-export default function App() {
-  const [symbol, setSymbol] = useState("X");
-  const [symbolList, setSymbolList] = useState(Array(9).fill(null));
-
-  /**
-   * 设置symbol
-   */
-  const writeSymbol = (event) => {
-    console.dir(event);
-    let node = event.target;
-    let index = node.dataset.index;
-    node.innerText = symbol;
-    symbolList[index] = symbol;
-    if (whoIsWinner()) {
-      alert(symbol + "is Winner");
-      return;
-    } else {
-      setSymbolList(symbolList);
-      if (symbol === "X") setSymbol("O");
-      if (symbol === "O") setSymbol("X");
-    }
-  };
-  /**
-   * 判断输赢
-   */
-  const whoIsWinner = () => {
-    let line = [
-      [0, 1, 2],
-      [3, 4, 5],
-      [6, 7, 8],
-      [0, 3, 6],
-      [1, 4, 7],
-      [2, 5, 8],
-      [0, 4, 8],
-      [2, 4, 6],
-    ];
-    let res = line.some((item) => {
-      if (
-        symbolList[item[0]] == null ||
-        symbolList[item[1]] == null ||
-        symbolList[item[2]] == null
-      ) {
-        return false;
-      } else if (
-        symbolList[item[0]] == symbolList[item[1]] &&
-        symbolList[item[0]] == symbolList[item[2]]
-      ) {
-        console.log(symbolList[item[0]]);
-        console.log(symbolList[item[1]]);
-        console.log(symbolList[item[2]]);
-        return true;
-      }
-
-      return false;
-    });
-    console.log(res);
-    return res;
-  };
-
-  return (
-    <div className="game_area">
-      <div onClick={writeSymbol} className="grid-top" data-index="0"></div>
-      <div onClick={writeSymbol} className="grid-top" data-index="1"></div>
-      <div onClick={writeSymbol} className="grid-top" data-index="2"></div>
-      <div onClick={writeSymbol} className="grid-middle" data-index="3"></div>
-      <div onClick={writeSymbol} className="grid-middle" data-index="4"></div>
-      <div onClick={writeSymbol} className="grid-middle" data-index="5"></div>
-      <div onClick={writeSymbol} className="grid-bottom" data-index="6"></div>
-      <div onClick={writeSymbol} className="grid-bottom" data-index="7"></div>
-      <div onClick={writeSymbol} className="grid-bottom" data-index="8"></div>
-    </div>
-  );
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <span></span>
+      </div>
+    );
+  }
 }
