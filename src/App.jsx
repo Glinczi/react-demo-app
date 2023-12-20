69a2028293b7c4948d6e39fdcc7df99c3ff9cd69
I'm XiaoZhi <gcz9956@outlook.com>
2023-12-20 22:37:15 +0800

    路由的基本使用 BrowserRouter、Link、Route

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,31 @@
 import React, { Component } from "react";
+import { Link, Route } from "react-router-dom";
+import Home from "./components/Home";
+import About from "./components/About";
 import "./App.css";
 
 export default class App extends Component {
   render() {
-    return <div className="app"></div>;
+    return (
+      <div className="app">
+        <div className="head-area">React Router Demo</div>
+        <div className="body-area flex">
+          <div className="left-nav flex flex-col" style={{ flex: 1 }}>
+            {/* 编写路由 */}
+            <Link className="nav-item" to="/home">
+              Home
+            </Link>
+            <Link className="nav-item" to="/about">
+              about
+            </Link>
+          </div>
+          <div className="right-content" style={{ flex: 2 }}>
+            {/* 注册路由 */}
+            <Route path="/home" component={Home} />
+            <Route path="/about" component={About} />
+          </div>
+        </div>
+      </div>
+    );
   }
 }
--- src/index.js
+++ src/index.js
@@ -4,6 +4,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 // 引入App组件
 import App from "./App";
+import { BrowserRouter } from "react-router-dom";
 // 渲染到页面
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(
+  // 需要放在最外层 整个App需要用一个路由器去管理
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+);
